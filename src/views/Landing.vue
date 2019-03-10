ddd8705c3c21603de8282a07ce67b10547e2ed01
Adam Meadows <Adam@jchendrick,com>
2019-03-10 22:48:28 +0000

    Words

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,6 +1,5 @@
 <template>
     <div>
-
         <div class="position-relative">
             <!-- shape Hero -->
             <section class="section-shaped my-0">
@@ -47,8 +46,9 @@
                                 <card class="border-0" hover shadow body-classes="py-5">
                                     <icon name="ni ni-check-bold" type="primary" rounded class="mb-4">
                                     </icon>
-                                    <h6 class="text-primary text-uppercase">Download Argon test</h6>
-                                    <p class="description mt-3">I am have worked in a plethora of different work environments from startup's to SME.</p>
+                                    <h6 class="text-primary text-uppercase">Proven success</h6>
+                                    <p class="description mt-3">I am have worked in a plethora of different work
+                                        environments from side projects, startup's to SME.</p>
                                     <div>
                                         <badge type="primary" rounded>design</badge>
                                         <badge type="primary" rounded>system</badge>
@@ -61,10 +61,9 @@
                                     <icon name="ni ni-istanbul" type="success" rounded class="mb-4">
                                     </icon>
                                     <h6 class="text-success text-uppercase">UK Based</h6>
-                                    <p class="description mt-3">Based in Milton Keynes, UK with quick access to
-                                        London </p>
-                                    <p class="description mt-3">With the ability to remote work anywhere in the
-                                        world</p>
+                                    <p class="description mt-3">Based in Milton Keynes, UK with easy access to
+                                        London. With the ability to remote work anywhere in the
+                                        world.</p>
                                     <div>
                                         <badge type="success" rounded>business</badge>
                                         <badge type="success" rounded>vision</badge>
@@ -77,7 +76,8 @@
                                     <icon name="ni ni-planet" type="warning" rounded class="mb-4">
                                     </icon>
                                     <h6 class="text-warning text-uppercase">Prepare Launch</h6>
-                                    <p class="description mt-3">With multiple projects under my belt I am well versed in deadlines and delivering high quality code. </p>
+                                    <p class="description mt-3">With multiple projects under my belt I am well versed in
+                                        deadlines and delivering high quality code. </p>
                                     <div>
                                         <badge type="warning" rounded>marketing</badge>
                                         <badge type="warning" rounded>product</badge>
@@ -85,44 +85,6 @@
                                     </div>
                                 </card>
                             </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <section class="section section-lg">
-            <div class="container">
-                <div class="row row-grid align-items-center">
-                    <div class="col-md-6 order-md-2">
-                        <img src="img/theme/promo-1.png" class="img-fluid floating">
-                    </div>
-                    <div class="col-md-6 order-md-1">
-                        <div class="pr-md-5">
-                            <icon name="ni ni-settings-gear-65" class="mb-5" size="lg" type="success" shadow
-                                  rounded></icon>
-                            <h3>Awesome features</h3>
-                            <p>The kit comes with three pre-built pages to help you get started faster. You can change
-                                the text and images and you're good to go.</p>
-                            <ul class="list-unstyled mt-5">
-                                <li class="py-2">
-                                    <div class="d-flex align-items-center">
-                                        <badge type="success" circle class="mr-3" icon="ni ni-settings-gear-65"></badge>
-                                        <h6 class="mb-0">Carefully crafted components</h6>
-                                    </div>
-                                </li>
-                                <li class="py-2">
-                                    <div class="d-flex align-items-center">
-                                        <badge type="success" circle class="mr-3" icon="ni ni-html5"></badge>
-                                        <h6 class="mb-0">Amazing page examples</h6>
-                                    </div>
-                                </li>
-                                <li class="py-2">
-                                    <div class="d-flex align-items-center">
-                                        <badge type="success" circle class="mr-3" icon="ni ni-satisfied"></badge>
-                                        <h6 class="mb-0">Super friendly support team</h6>
-                                    </div>
-                                </li>
-                            </ul>
                         </div>
                     </div>
                 </div>
@@ -189,30 +151,6 @@
                 </div>
             </div>
         </section>
-        <section class="section section-lg pt-0">
-            <div class="container">
-                <card gradient="warning"
-                      no-body
-                      shadow-size="lg"
-                      class="border-0">
-                    <div class="p-5">
-                        <div class="row align-items-center">
-                            <div class="col-lg-8">
-                                <h3 class="text-white">We made website building easier for you.</h3>
-                                <p class="lead text-white mt-3">I will be the leader of a company that ends up being
-                                    worth billions of dollars, because I got the answers. I understand culture.</p>
-                            </div>
-                            <div class="col-lg-3 ml-lg-auto">
-                                <base-button tag="a" href="https://www.creative-tim.com/product/vue-argon-design-system"
-                                             type="white" block size="lg">
-                                    Download Vue
-                                </base-button>
-                            </div>
-                        </div>
-                    </div>
-                </card>
-            </div>
-        </section>
         <section class="section section-shaped my-0 overflow-hidden">
             <div class="shape shape-style-3 bg-gradient-default shape-skew">
                 <span></span>
@@ -230,21 +168,15 @@
                     </div>
                 </div>
                 <div class="row row-grid mt-5">
-                    <div class="col-lg-4">
+                    <div class="col-lg-6">
                         <icon name="ni ni-settings" size="lg" gradient="white" shadow round color="primary"></icon>
                         <h5 class="text-white mt-3">Building tools</h5>
-                        <p class="text-white mt-3">Some quick example text to build on the card title and make up the
-                            bulk of the card's content.</p>
+                        <p class="text-white mt-3">I am well versed in the most modern cloud technologies from AWS and
+                            have over 10 years of programming experience.</p>
                     </div>
-                    <div class="col-lg-4">
+                    <div class="col-lg-6">
                         <icon name="ni ni-ruler-pencil" size="lg" gradient="white" shadow round color="primary"></icon>
                         <h5 class="text-white mt-3">Grow your market</h5>
-                        <p class="text-white mt-3">Some quick example text to build on the card title and make up the
-                            bulk of the card's content.</p>
-                    </div>
-                    <div class="col-lg-4">
-                        <icon name="ni ni-atom" size="lg" gradient="white" shadow round color="primary"></icon>
-                        <h5 class="text-white mt-3">Launch time</h5>
                         <p class="text-white mt-3">Some quick example text to build on the card title and make up the
                             bulk of the card's content.</p>
                     </div>
@@ -256,8 +188,14 @@
                 <div class="row justify-content-center mt--300">
                     <div class="col-lg-8">
                         <card gradient="secondary" shadow body-classes="p-lg-5">
-                            <h4 class="mb-1">Want to work with us?</h4>
-                            <p class="mt-0">Your project is very important to us.</p>
+                            <h4 class="mb-1">Want to contact me?</h4>
+                            <base-alert type="danger" dismissible v-if="messageNYI">
+                                <span class="alert-inner--icon"><i class="ni ni-like-2"></i></span>
+                                <span class="alert-inner--text">Not Yet Implemented</span>
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close" @click="dismissNYI">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </base-alert>
                             <base-input class="mt-5"
                                         alternative
                                         placeholder="Your name"
@@ -271,7 +209,8 @@
                                     <textarea class="form-control form-control-alternative" name="name" rows="4"
                                               cols="80" placeholder="Type a message..."></textarea>
                             </base-input>
-                            <base-button type="default" round block size="lg">
+                            <base-button type="default" round block size="lg"
+                                         @click="sendMessage()">
                                 Send Message
                             </base-button>
                         </card>
@@ -286,6 +225,21 @@
 <script>
     export default {
         name: "home",
-        components: {}
+        components: {},
+        data() {
+            return {
+                messageNYI: false
+            }
+        },
+        methods: {
+            sendMessage() {
+                console.log("send message")
+                this.messageNYI = true;
+            },
+            dismissNYI(){
+                debugger
+                this.messageNYI = false;
+            }
+        }
     };
 </script>
