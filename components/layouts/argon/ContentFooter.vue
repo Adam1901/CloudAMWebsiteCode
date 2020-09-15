92ce47cee24c53d1b1ee25495c8d39fa9835d728
Adam Meadows <Adam@jchendrick,com>
2020-09-16 00:07:45 +0100

    UI content

--- components/layouts/argon/ContentFooter.vue
+++ components/layouts/argon/ContentFooter.vue
@@ -3,7 +3,7 @@
     <div class="row align-items-center justify-content-lg-between">
       <div class="col-lg-6">
         <div class="copyright text-center text-lg-left text-muted">
-          © {{year}} <a href="https://www.creative-tim.com" class="font-weight-bold ml-1" target="_blank" rel="noopener">Creative Tim</a> &
+          © {{year}} CloudAM, <a href="https://www.creative-tim.com" class="font-weight-bold ml-1" target="_blank" rel="noopener">Creative Tim</a> &
           <a href="https://www.binarcode.com?ref=creative-tim" class="font-weight-bold ml-1" target="_blank" rel="noopener">Binar Code</a>
         </div>
       </div>
@@ -13,13 +13,7 @@
             <a href="https://www.creative-tim.com" class="nav-link" target="_blank" rel="noopener">Creative Tim</a>
           </li>
           <li class="nav-item">
-            <a href="https://www.creative-tim.com/presentation" class="nav-link" target="_blank" rel="noopener">About Us</a>
-          </li>
-          <li class="nav-item">
-            <a href="http://blog.creative-tim.com" class="nav-link" target="_blank" rel="noopener">Blog</a>
-          </li>
-          <li class="nav-item">
-            <a href="https://www.creative-tim.com/license" class="nav-link" target="_blank" rel="noopener">License</a>
+            <a href="https://www.github.com/adam1901" class="nav-link" target="_blank" rel="noopener">Github</a>
           </li>
         </ul>
       </div>
